337c6315e91cad29b4c4fdfb9377fe4e31d037ce
GlakovaA <22109@bk.ru>
2022-11-13 11:27:08 +0300

    первый коммит

---
+++ src/main/java/pro/sky/kursovaja2kurs/Kursovaja2KursApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.kursovaja2kurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kursovaja2KursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kursovaja2KursApplication.class, args);
+	}
+
+}
